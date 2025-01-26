b105afbb2168d61429d8088ad9aaf3155458da5c
vlad-terehoff <tereh_off95@mail.ru>
2025-01-26 06:23:24 +0500

    .

--- web/src/check_request/service.py
+++ web/src/check_request/service.py
@@ -2,6 +2,7 @@ from src.base.db_settings.session import ISession
 from src.check_request.repository import check_request_repository, CheckWalletRepository
 from src.check_request.schemas import CheckWalletDTO
 from src.check_request.utils import get_account_info
+from fastapi import HTTPException, status
 
 
 class CheckWalletService:
@@ -13,7 +14,13 @@ class CheckWalletService:
         info = await get_account_info(dto.wallet)
 
         await self.repository.login_request_wallet_information(session=session, dto=dto)
-        return info
+
+        if info:
+            return info
+
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Запрашиваемый кошелек либо не коректен либо еще не активирован")
 
     async def get_all(self, session: ISession):
         result = await self.repository.get_all(session=session)
